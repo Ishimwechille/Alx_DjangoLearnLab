28ec33a61df351270b10d901bd736f28f2a75839
Ishimwechille <012220538@isbatuniversity.com>
2025-11-13 15:52:44 +0300

    addd

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import permission_required, login_required
+from .models import Article
 
 #my inclusions
 from django.http import HttpResponse
@@ -7,3 +10,44 @@ from django.http import HttpResponse
 def index(request):
     response = "Welcome to the Book shelf 📚"
     return HttpResponse(response)
+
+
+@login_required
+@permission_required('relationship_app.can_view', raise_exception=True)
+def article_list(request):
+    articles = Article.objects.all()
+    return render(request, 'relationship_app/article_list.html', {'articles': articles})
+
+@login_required
+@permission_required('relationship_app.can_create', raise_exception=True)
+def create_article(request):
+    if request.method == "POST":
+        form = ArticleForm(request.POST)
+        if form.is_valid():
+            article = form.save(commit=False)
+            article.author = request.user
+            article.save()
+            return redirect('article_list')
+    else:
+        form = ArticleForm()
+    return render(request, 'relationship_app/create_article.html', {'form': form})
+
+@login_required
+@permission_required('relationship_app.can_edit', raise_exception=True)
+def edit_article(request, pk):
+    article = get_object_or_404(Article, pk=pk)
+    if request.method == "POST":
+        form = ArticleForm(request.POST, instance=article)
+        if form.is_valid():
+            form.save()
+            return redirect('article_list')
+    else:
+        form = ArticleForm(instance=article)
+    return render(request, 'relationship_app/edit_article.html', {'form': form})
+
+@login_required
+@permission_required('relationship_app.can_delete', raise_exception=True)
+def delete_article(request, pk):
+    article = get_object_or_404(Article, pk=pk)
+    article.delete()
+    return redirect('article_list')
