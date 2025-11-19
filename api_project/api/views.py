08e349d729753a4d08284632c8a520ad1f57ebc7
Ishimwechille <155310525+Ishimwechille@users.noreply.github.com>
2025-11-19 17:44:24 +0300

    Update views.py
    my work

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -13,4 +13,6 @@ class BookList(generics.ListAPIView):
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
+
 
