d425a5d6e3b43cbb619f5734e66ef25076134fb3
Ishimwechille <155310525+Ishimwechille@users.noreply.github.com>
2025-11-19 17:35:51 +0300

    Update urls.py

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,6 +1,19 @@
 # api/urls.py
-from django.urls import path
-from .views import BookList
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import BookList, 
+from .views import BookViewSet.
+
+router = DefaultRouter()
+router.register(r'books_all', BookViewSet, basename='book_all')
+
+urlpatterns = [
+    path('books/', BookList.as_view(), name='book-list'),  # Optional: keep if you want a separate list view
+    path('', include(router.urls)),  # Auto-generates all CRUD routes
+]
+
+
+
 
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),
