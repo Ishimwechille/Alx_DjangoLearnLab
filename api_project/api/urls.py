f2ed0307862e17a9362a55f90129f957fb14b50e
Ishimwechille <155310525+Ishimwechille@users.noreply.github.com>
2025-11-19 17:51:09 +0300

    Add API token authentication URL to urlpatterns
    work

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -8,6 +8,12 @@ from rest_framework.authtoken.views import obtain_auth_token
 
 router = DefaultRouter()
 router.register(r'books_all', BookViewSet, basename='book_all')
+urlpatterns = [
+    ...
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
+]
+
+
 
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),  # Optional: keep if you want a separate list view
