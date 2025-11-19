f761d3e32567c7d286c7686fee6ddb57db40bff6
Ishimwechille <155310525+Ishimwechille@users.noreply.github.com>
2025-11-19 17:42:59 +0300

    Update settings.py
    update

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -37,8 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
-     'api',
+   'rest_framework',
+    'rest_framework.authtoken',
+    'api',
+
+
 
 
 
@@ -126,3 +129,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
