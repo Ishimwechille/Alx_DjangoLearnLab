75cd982a9d643c7fa51d61b082b0af7aee8bf752
Ishimwechille <155310525+Ishimwechille@users.noreply.github.com>
2025-11-25 21:34:22 +0300

    Add REST framework configuration to settings
    Added REST framework settings to Django project.

--- advanced-api-project/advanced_api_project/settings.py
+++ advanced-api-project/advanced_api_project/settings.py
@@ -40,6 +40,14 @@ INSTALLED_APPS = [
      'rest_framework',
     'api',
 ]
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+        'rest_framework.filters.SearchFilter',
+        'rest_framework.filters.OrderingFilter',
+    ]
+}
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -122,4 +130,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
