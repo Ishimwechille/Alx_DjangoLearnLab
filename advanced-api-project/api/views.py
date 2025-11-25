d95b90c50cb10b95d1ffed5c22299efa0220df09
Ishimwechille <155310525+Ishimwechille@users.noreply.github.com>
2025-11-25 21:11:16 +0300

    Update views.py
    see what is happening

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+/books/                 → List all books
+/books/<int:pk>/        → Get a single book
+/books/create/          → Create
+/books/<int:pk>/update/ → Update
+/books/<int:pk>/delete/ → Delete
+
