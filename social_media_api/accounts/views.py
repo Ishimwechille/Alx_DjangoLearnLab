3ed7f9ca3fcb447deeb206cc1b318d89381cc7d1
Ishimwechille <155310525+Ishimwechille@users.noreply.github.com>
2025-12-08 21:04:08 +0300

    Update views.py

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,10 +1,9 @@
-from django.shortcuts import render
-
-# Create your views here.
-from rest_framework.authtoken.models import Token
-from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
 from django.contrib.auth import authenticate
+from rest_framework.permissions import IsAuthenticated
+
 from .serializers import UserSerializer
 from django.contrib.auth import get_user_model
 
@@ -13,20 +12,29 @@ User = get_user_model()
 class RegisterView(APIView):
     def post(self, request):
         serializer = UserSerializer(data=request.data)
-        if serializer.is_valid():
-            user = serializer.save()
-            token, created = Token.objects.get_or_create(user=user)
-            return Response({"token": token.key, "user": serializer.data})
-        return Response(serializer.errors, status=400)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        token = Token.objects.get(user=user)
+        return Response({"token": token.key, "user": serializer.data})
+
 
 class LoginView(APIView):
     def post(self, request):
         username = request.data.get("username")
         password = request.data.get("password")
+
         user = authenticate(username=username, password=password)
 
-        if user:
-            token, created = Token.objects.get_or_create(user=user)
-            serializer = UserSerializer(user)
-            return Response({"token": token.key, "user": serializer.data})
-        return Response({"error": "Invalid credentials"}, status=400)
+        if not user:
+            return Response({"error": "Invalid username or password"}, status=400)
+
+        token, _ = Token.objects.get_or_create(user=user)
+        return Response({"token": token.key})
+
+
+class ProfileView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        serializer = UserSerializer(request.user)
+        return Response(serializer.data)
