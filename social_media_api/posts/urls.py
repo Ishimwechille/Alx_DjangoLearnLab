f8bf6aaa1c7b106b46417c6ce1df835203de7636
Ishimwechille <155310525+Ishimwechille@users.noreply.github.com>
2025-12-09 19:05:25 +0300

    Update urls.py

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,5 +1,5 @@
 from rest_framework.routers import DefaultRouter
-from django.urls import path, include
+from django.urls import include, path
 from .views import PostViewSet, CommentViewSet
 
 router = DefaultRouter()
