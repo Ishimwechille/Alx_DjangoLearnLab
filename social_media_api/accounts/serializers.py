18915c3ecff998c4c8dcdb9ca53931f096b2b20f
Ishimwechille <155310525+Ishimwechille@users.noreply.github.com>
2025-12-09 20:45:27 +0300

    Update serializers.py

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,12 +1,7 @@
-from rest_framework import serializers
-from django.contrib.auth import get_user_model
-from .models import Post, Comment
+# accounts/serializers.py
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from rest_framework.authtoken.models import Token
-
-
-
 
 User = get_user_model()
 
@@ -18,6 +13,7 @@ class RegisterSerializer(serializers.ModelSerializer):
         fields = ['username', 'email', 'password', 'bio', 'profile_picture']
 
     def create(self, validated_data):
+        # Must explicitly use create_user
         user = User.objects.create_user(
             username=validated_data['username'],
             email=validated_data.get('email'),
@@ -31,68 +27,3 @@ class RegisterSerializer(serializers.ModelSerializer):
 class LoginSerializer(serializers.Serializer):
     username = serializers.CharField()
     password = serializers.CharField(write_only=True)
-
-# -------------------------
-# Comment Serializer
-# -------------------------
-class CommentSerializer(serializers.ModelSerializer):
-    author = serializers.StringRelatedField(read_only=True)
-    author_id = serializers.PrimaryKeyRelatedField(
-        source="author",
-        queryset=User.objects.all(),
-        write_only=True,
-        required=False
-    )
-
-    class Meta:
-        model = Comment
-        fields = [
-            "id",
-            "post",
-            "content",
-            "author",
-            "author_id",
-            "created_at",
-            "updated_at"
-        ]
-        read_only_fields = ["id", "author", "created_at", "updated_at"]
-
-
-# -------------------------
-# Post List Serializer
-# -------------------------
-class PostListSerializer(serializers.ModelSerializer):
-    author = serializers.StringRelatedField(read_only=True)
-    comments_count = serializers.IntegerField(source="comments.count", read_only=True)
-
-    class Meta:
-        model = Post
-        fields = [
-            "id",
-            "title",
-            "author",
-            "comments_count",
-            "created_at",
-            "updated_at"
-        ]
-
-
-# -------------------------
-# Post Detail Serializer
-# -------------------------
-class PostDetailSerializer(serializers.ModelSerializer):
-    author = serializers.StringRelatedField(read_only=True)
-    comments = CommentSerializer(many=True, read_only=True)
-
-    class Meta:
-        model = Post
-        fields = [
-            "id",
-            "title",
-            "content",
-            "author",
-            "comments",
-            "created_at",
-            "updated_at",
-        ]
-        read_only_fields = ["author", "created_at", "updated_at"]
