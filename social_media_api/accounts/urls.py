3568145929b4ce2eb7d25ec41a54dedc250b0e0d
Ishimwechille <155310525+Ishimwechille@users.noreply.github.com>
2025-12-09 19:38:25 +0300

    Update urls.py

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -1,13 +1,10 @@
-
-from django.urls import path, include
-from rest_framework import routers
-
-from .views import RegisterUserView, LoginUserView, UserProfileView
+from .views import FeedView
 
 urlpatterns = [
-    path('register/', RegisterUserView.as_view, name='register'),
-    path('login/', LoginUserView.as_view, name='login'),
+    path('register/', RegisterUserView.as_view(), name='register'),
+    path('login/', LoginUserView.as_view(), name='login'),
     path('profile/', UserProfileView.as_view(), name='profile'),
     path('follow/<int:user_id>/', UserProfileView.as_view(), name='follow'),
     path('unfollow/<int:user_id>/', UserProfileView.as_view(), name='unfollow'),
+    path('feed/', FeedView.as_view(), name='feed'),  # ✅ New route
 ]
