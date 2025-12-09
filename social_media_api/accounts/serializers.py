c389b1507ef4995d654d3c5e044ee118d0e4c906
Ishimwechille <155310525+Ishimwechille@users.noreply.github.com>
2025-12-09 20:43:55 +0300

    Update serializers.py

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,30 +1,36 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from .models import Post, Comment
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from rest_framework.authtoken.models import Token
+
+
+
 
 User = get_user_model()
 
-
-# --------------------------------------
-# User Serializer (for followers feature)
-# --------------------------------------
-class UserSerializer(serializers.ModelSerializer):
-    followers_count = serializers.IntegerField(source="followers.count", read_only=True)
-    following_count = serializers.IntegerField(source="following.count", read_only=True)
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
 
     class Meta:
         model = User
-        fields = [
-            "id",
-            "username",
-            "email",
-            "first_name",
-            "last_name",
-            "followers_count",
-            "following_count",
-        ]
-        read_only_fields = ["id", "followers_count", "following_count"]
+        fields = ['username', 'email', 'password', 'bio', 'profile_picture']
 
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data.get('email'),
+            password=validated_data['password'],
+            bio=validated_data.get('bio', ''),
+            profile_picture=validated_data.get('profile_picture')
+        )
+        Token.objects.create(user=user)
+        return user
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
 
 # -------------------------
 # Comment Serializer
