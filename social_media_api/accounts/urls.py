7858a9eaf5364e1b501dfe78d38288122841b235
Ishimwechille <155310525+Ishimwechille@users.noreply.github.com>
2025-12-09 20:53:37 +0300

    Update urls.py

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -1,9 +1,11 @@
 
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import PostViewSet, CommentViewSet, FeedView
-from django.contrib import admin
-from django.urls import path, include
+from .views import PostViewSet, CommentViewSet, FeedView,
+from django.contrib import admin,
+from django.urls import path, include,
+from .views import RegisterView, LoginView,
+
 
 # DRF router for posts and comments
 router = DefaultRouter()
@@ -14,6 +16,10 @@ urlpatterns = [
     path('', include(router.urls)),
     path('feed/', FeedView.as_view(), name='feed'),  # ✅ This is the required feed route
     path('unfollow/<int:user_id>/', UserProfileView.as_view(), name='unfollow'),
-    path('admin/', admin.site.urls),
-    path('accounts/', include('accounts.urls')),  # include accounts app URLs
+      # include accounts app URLs
+     path('register/', RegisterView.as_view(), name='register'),  # /register/
+    path('login/', LoginView.as_view(), name='login'),           # /login/
+    path('profile/', lambda request: None, name='profile'),   
+    path('accounts/', include('accounts.urls')),
+
 ]
