2bdba67863556426964b24e7010037e81315e3ad
Ishimwechille <155310525+Ishimwechille@users.noreply.github.com>
2025-12-09 19:08:51 +0300

    Update urls.py

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -1,7 +1,16 @@
-from django.urls import path
-from .views import RegisterView, LoginView
+from rest_framework.routers import DefaultRouter
+from django.urls import include, path
+from .views import UserViewSet, RegisterView, LoginView
+
+# DRF router for UserViewSet (read, follow, unfollow, followers, following)
+router = DefaultRouter()
+router.register(r"accounts", UserViewSet, basename="user")
 
 urlpatterns = [
-    path('register/', RegisterView.as_view(), name='register'),
-    path('login/', LoginView.as_view(), name='login'),
+    # Router URLs for UserViewSet
+    path("", include(router.urls)),
+
+    # Manual URLs for registration and login
+    path("register/", RegisterView.as_view(), name="register"),
+    path("login/", LoginView.as_view(), name="login"),
 ]
