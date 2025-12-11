b121b5ed1cc7435c45fceeca76f9d622b97a8772
Ishimwechille <155310525+Ishimwechille@users.noreply.github.com>
2025-12-11 17:45:39 +0300

    Update urls.py

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -7,7 +7,8 @@ router.register(r"posts", PostViewSet, basename="post")
 router.register(r"comments", CommentViewSet, basename="comment")
 
 urlpatterns = [
-    path("", include(router.urls)),
+   path('', include(router.urls)),
+    path('feed/', FeedView.as_view(), name='feed'),
     path('posts/<int:pk>/like/', LikePostView.as_view(), name='like-post'),
-    path('posts/<int:pk>/unlike/', UnlikePostView.as_view(), name='unlike-post'),
+    path('posts/<int:pk>/unlike/', UnlikePostView.as_view(), name='unlike-post')
 ]
